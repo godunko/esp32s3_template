b6d9aa66c209d05e99d3b5a3671d62e7cc999a03
Vadim Godunko <vgodunko@gmail.com>
2026-04-25 13:48:54 +0400

    Cleanup main and restart MCU before exit.

--- source/main.adb
+++ source/main.adb
@@ -8,18 +8,12 @@ pragma Warnings (On, "is an internal GNAT unit");
 --  `System.Tasking.Initialization` should be with-ed to setup task-safe soft
 --  links, which is required due to use of FreeRTOS by ESP-IDF.
 
-with A0B.Types;
 with Ada.Text_IO;
 
 procedure Main is
-   use type A0B.Types.Unsigned_32;
 
-   function F return String;
-
-   function F return String is ("def");
-
-   Count : A0B.Types.Unsigned_32 := 0;
-   S     : String := "abc" & F & A0B.Types.Unsigned_32'Image (Count);
+   procedure ESP_Restart
+     with Import, Convention => C, External_Name => "esp_restart";
 
 begin
    Ada.Text_IO.New_Line;
@@ -32,12 +26,10 @@ begin
      ("Feel free to replace it by your application!");
    Ada.Text_IO.New_Line;
 
-   for J in 1 .. 1_000 loop
-      Count := @ + 1;
-   end loop;
+   delay 1.0;
 
-   Ada.Text_IO.Put_Line (S & A0B.Types.Unsigned_32'Image (Count));
-   Ada.Text_IO.New_Line;
-   Ada.Text_IO.Put_Line ("Excellent, done!");
-   Ada.Text_IO.New_Line;
+   Ada.Text_IO.Put_Line ("Restarting...");
+
+   ESP_Restart;
+   --  Restart to exit from Qemu in CI.
 end Main;
